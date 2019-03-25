9306b725d211645998f36098cbe5d0853bbb4f3f
Salomon Dion <dev.mrdion@gmail.com>
2019-03-25 18:50:24 +0000

    create http request class

---
+++ src/Elephanto.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Elephanto;
+
+use Elephanto\Http\HttpClient;
+
+/**
+ * Elephanto class
+ * @author Salomon Dion (dev.mrdion@gmail.com)
+ */
+class Elephanto
+{
+    public static function fetch(string $url, array $conf = [])
+    {
+        $method = $conf['method'] ?? 'GET';
+
+        switch ($method) {
+            case 'POST':
+                return static::post($url, $conf);
+            default:
+                return static::get($url, $conf);
+                break;
+        }
+    }
+
+    public static function get(string $url, array $conf = [])
+    {
+        $http = new HttpClient;
+        return $http->get($url, $conf);
+    }
+
+    public static function post(string $url, $data = null, array $conf = [])
+    {
+        $http = new HttpClient;
+        return $http->post($url, $data, $conf);
+    }
+
+    public static function create(array $options = [])
+    {
+        return new HttpClient($options);
+    }
+}
